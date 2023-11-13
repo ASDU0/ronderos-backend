86fe380aaa9b3e44cca4dd5780990445adb632f4
joel <52236148+joelinfo19@users.noreply.github.com>
2023-11-13 00:12:40 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import {DATA_BASE_CONFIGURATION} from "./configuration";
 async function bootstrap() {
   console.log("DATA"+DATA_BASE_CONFIGURATION.mongoConnectionString);
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
